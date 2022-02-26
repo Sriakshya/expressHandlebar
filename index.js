315be0d13f86cd21092127106edd34338ed48b96
Sriakshya <sriakshyasrinivasan00@gmail.com>
2022-02-26 21:42:30 +0530

    nothing

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
+const { engine } = require ('express-handlebars');
+
+const handlebars = require('express-handlebars');
+app.set('view engine', 'handlebars');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
+
+// app.engine('handlebars', handlebars({
+// layoutsDir: '/views/layouts',
+// }));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-    res.send("Hello world!");
-})
 
-app.listen(port, () => {
-    console.log("Application is listening on port " + port);
-})
+res.render('main', {layout : 'index'});
+});
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
